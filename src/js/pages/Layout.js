16b0faf6e357b5800c691a52174b862b188e89e9
Hideto Yasunori <hideto0118yasunori@gmail.com>
2019-12-10 20:43:12 -0800

    Add query string

--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Link, withRouter } from "react-router-dom";
+import { NavLink, Link, withRouter } from "react-router-dom";
 
 
 class Layout extends React.Component {
@@ -13,9 +13,9 @@ class Layout extends React.Component {
       <div>
         <h1>KillerNews.net</h1>
         {this.props.children}
-        <Link to="/archives/some-other-articles" class="btn btn-warning">archives (some other articles)</Link>
-        <Link to="/archives" class="btn btn-danger">archives</Link>
-        <Link to="/settings/main" class="btn btn-success">settings</Link>
+        <Link to="/archives/some-other-articles?date=yesterday&filter=none" class="btn btn-warning">archives (some other articles)</Link>
+        <Link to="/archives?date=today&filter=hot" class="btn btn-danger">archives</Link>
+        <NavLink to="/settings/main" className="btn btn-success">settings</NavLink>
         <Link to="/settings/extra" class="btn btn-success">settings (extra)</Link>
         <button class="btn btn-info" onClick={this.navigate.bind(this)}>featured</button>
       </div>
